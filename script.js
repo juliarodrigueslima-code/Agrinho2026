2fc9c8bb0e89a8a8afd2d96ae6108f5bdecb5cd5
juliarodrigueslima-code <julia.rodrigues.lima@escola.pr.gov.br>
2026-07-07 16:52:36 -0300

    agrinho2026

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ console.log("Projeto Agrinho 2026 carregado!");
 window.onload = function(){
 alert("Bem-vindo ao projeto Agrinho 2026! 🌱");
 }
+function mostrarNome(){
+
+let nome = document.getElementById("nome").value;
+
+if(nome == ""){
+
+document.getElementById("mensagem").innerHTML = "Por favor, digite seu nome.";
+
+}else{
+
+document.getElementById("mensagem").innerHTML =
+"Seja bem-vindo(a), <strong>" + nome + "</strong>! 🌱";
+
+}
+
+}
